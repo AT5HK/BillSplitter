233e1a2693b0395696848b13e6acccb664ac2b7f
Auston <auston.salvana@live.com>
2015-07-11 15:02:18 -0700

    Initial Commit

---
+++ BillSplitter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BillSplitter
+//
+//  Created by Auston Salvana on 7/11/15.
+//  Copyright (c) 2015 ASolo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
